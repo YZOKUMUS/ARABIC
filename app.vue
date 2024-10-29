8c23467de3a1e42f28bbcd25d413f2962cb4af09
Yusuf <ziya.okumus@gmail.com>
2024-10-29 23:31:34 +0300

    23 31 saatinde commit kaydı

--- app.vue
+++ app.vue
@@ -1,27 +1,32 @@
 <template>
   <v-app>
+    <!-- Yan Menü (Navigation Drawer) -->
     <v-navigation-drawer app v-model="drawer" permanent>
       <v-list>
         <v-list-item
           v-for="(item, index) in navItems"
           :key="index"
           :to="item.to"
+          @click="navigateTo(item.to)"
         >
           <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
+    <!-- Üst Menü (App Bar) -->
     <v-app-bar app>
       <v-btn icon @click="toggleDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
-      <v-toolbar-title>ARAPÇA ÖĞRENİYORUM</v-toolbar-title>
+      <v-toolbar-title>ARAPÇA ÖĞRENİYORUM بِسْــــــــــــــــــــــمِ اﷲِارَّحْمَنِ ارَّحِيم</v-toolbar-title>
       <v-spacer />
     </v-app-bar>
 
+    <!-- İçerik Alanı -->
     <v-container>
       <v-container class="main-content mt-15">
+        <!-- Dinamik Sayfa İçeriği -->
         <NuxtPage />
       </v-container>
     </v-container>
@@ -29,6 +34,8 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
+
 export default {
   data() {
     return {
@@ -39,12 +46,21 @@ export default {
         { title: 'EMSİLEİ MUHTELİFE', to: '/ziya' },
         { title: '13 FİİL ÇEKİMİ', to: '/contact' },
         { title: 'ESMAUL HUSNA', to: '/sayfa' },
+        { title: 'KURANDA GEÇEN FİİLLER', to: '/kurandagecenfiiller' },
       ],
     };
   },
   methods: {
     toggleDrawer() {
       this.drawer = !this.drawer;
+    },
+    navigateTo(route) {
+      // Vue Router'ı kullanarak belirli bir yola yönlendiriyoruz
+      const router = useRouter();
+      router.push(route);
+      
+      // Yan menüyü kapatıyoruz
+      this.drawer = false;
     },
   },
 };
--- pages/kurandagecenfiiller.vue
+++ pages/kurandagecenfiiller.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-   <excel-viewer :excelFile="uploadedFile" :shuffle="false" />
+    <excel-viewer :excelFile="uploadedFile" :shuffle="false" />
   </div>
 </template>
 
@@ -16,7 +16,7 @@ export default {
   },
   methods: {
     async loadFile() {
-      const response = await fetch('./mezidfiiller1.xlsx');
+      const response = await fetch('./kurandagecenfiiller.xlsx');
       const arrayBuffer = await response.arrayBuffer();
       this.uploadedFile = arrayBuffer; // ArrayBuffer olarak excelFile prop'una gönderiliyor
     },
