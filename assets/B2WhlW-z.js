a1b9d0eba9e9c086ba41612a73046ba6171dac0e
Yusuf <ziya.okumus@gmail.com>
2024-11-14 17:58:28 +0300

    Updates

--- assets/B2WhlW-z.js
+++
@@ -1,1 +0,0 @@
-import{J as e,_ as a,v as t,x as n,t as r}from"./DMD88gNX.js";const c=e("/aksamiseba.JPG"),i={};function o(l,s){return r(),t("div",null,s[0]||(s[0]=[n("img",{src:c,alt:"Yüklenen Resim",class:"image-fit",width:"100%"},null,-1)]))}const _=a(i,[["render",o]]);export{_ as default};
