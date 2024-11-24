b11eb16580879953a31889078b0e68e2be9f7752
Yusuf <ziya.okumus@gmail.com>
2024-11-24 11:40:45 +0300

    Updates

--- assets/DYVCm7Tx.js
+++
@@ -1,1 +0,0 @@
-import{J as e,_ as a,v as t,x as n,t as r}from"./CCS6xpA3.js";const c=e("/aksamiseba.JPG"),i={};function o(l,s){return r(),t("div",null,s[0]||(s[0]=[n("img",{src:c,alt:"Yüklenen Resim",class:"image-fit",width:"100%"},null,-1)]))}const _=a(i,[["render",o]]);export{_ as default};
